38cfd73dff489113691e8b0f2fdd5b5c355dde51
Richard Anderzon Garcia <rg911068@gmail.com>
2026-06-23 18:57:52 +0000

    agregacion de get line

--- app/Main.hs
+++ app/Main.hs
@@ -6,10 +6,12 @@ areaCirculo radio = pi * radio * radio
 
 main :: IO ()
 main = do
-    putStrLn "¡Hola mundo desde Haskell en la nube!"
-    putStrLn "Hola a todo"
+    putStrLn "¡Hola mundo desde Haskell"
+    putStrLn "introduce el radio del circulo"
+    radio_ <- getLine
     putStrLn ""
     putStrLn "Vamos a calcular el área de un círculo:"
-    let radio = 5.0
+    let radio = read radio_ :: Double
     putStrLn ("Radio: " ++ show radio)
-    putStrLn ("Área: " ++ show (areaCirculo radio))
+    putStrLn ("Área del circulo es: " ++ show (areaCirculo radio))
+    putStrLn ""
